51874f96c7e4c271738315364df4e10c11fa70a8
KedarMH <kedarmh19@gmail.com>
2023-01-04 13:37:45 +0530

    Transaction form design using MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import { useState, useEffect } from 'react'
+import AppBar from './components/AppBar.js'
+import TransactionForm from './components/TransactionForm.js'
+
 
 
 const InitialForm = {
@@ -38,13 +41,15 @@ function App() {
     });
 
     if (res.ok) {
-      setForm (InitialForm);
+      setForm(InitialForm);
       fetchTransctions();
     }
   }
 
   return (
     <div>
+      <AppBar />
+      <TransactionForm/>
       <form onSubmit={handleSubmit}>
         <input
           type="number"
