d672e9a1103a4f508944089fabf2bb155aa19e6e
KedarMH <kedarmh19@gmail.com>
2023-01-07 01:05:31 +0530

    Login User part2

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,18 +3,20 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-
+import Cookies from 'js-cookie'
 
 
 
 export default function Login() {
+  const navigate = useNavigate();
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -30,8 +32,12 @@ export default function Login() {
         "content-type": "application/json"
       }
     });
+
+    const { token } = await res.json();
+
     if (res.ok) {
-      console.log("done");
+      Cookies.set('token', token)
+      navigate('/');
     }
 
   };
